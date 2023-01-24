f855fec979ebe6df82bb3e103ed9ea9b5563d8a2
Alvaro05 <202100765@alu.comillas.edu>
2023-01-24 20:51:26 +0100

    He creado un fichero con la clase marcador, que tiene dos atributos: valor e Id, y sus correspondientes funciones

---
+++ Test bench/Display Alvaro/Proyecto Pantalla/Marcador.cpp
@@ -0,0 +1,25 @@
+class Marcador {
+  private:
+    float valor;
+    int Id;
+
+  public:
+    Marcador(){
+      valor = 0.0;
+      Id = 00000000000000000000000000000000;
+    }
+    void setValor(float v){
+      valor = v;
+    }
+    float getValor(){
+      return valor;
+    }
+
+    void setId(int id){
+      Id = id;
+    }
+
+    int getId(){
+      return id
+    }
+};
