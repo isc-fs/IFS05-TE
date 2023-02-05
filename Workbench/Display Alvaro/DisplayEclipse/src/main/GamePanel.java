3d3d5c21c118050ef0da28c3bbac5e80d50ae93f
Alvaro05 <202100765@alu.comillas.edu>
2023-02-05 19:57:37 +0100

    Pequeñas mejoras

--- Workbench/Display Alvaro/DisplayEclipse/src/main/GamePanel.java
+++ Workbench/Display Alvaro/DisplayEclipse/src/main/GamePanel.java
@@ -5,8 +5,10 @@ import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Font;
 
 import javax.swing.JPanel;
+import javax.swing.JLabel;
 import javax.swing.ImageIcon;
 
 
@@ -19,6 +21,14 @@ public class GamePanel extends JPanel implements Runnable{
 	
 	Thread gameThread;
 	
+	Color fondo = new Color(25, 26, 30);
+	Color ISC = new Color(251, 187, 28);
+	Color ISCgreen = new Color(92, 132, 28);
+	
+	Font font = new Font("Helvetica SemiBold", Font.BOLD, 120);
+	Font font2 = new Font("Helvetica SemiBold", Font.BOLD, 57);
+
+	
 	final int screenWidth = tileSize * maxScreenCol;
 	final int screenHeight = tileSize * maxScreenRow;
 	
@@ -28,7 +38,7 @@ public class GamePanel extends JPanel implements Runnable{
 	public GamePanel()
 	{
 		this.setPreferredSize(new Dimension(screenWidth, screenHeight));
-		this.setBackground(Color.darkGray);
+		this.setBackground(fondo);
 		this.setDoubleBuffered(true);
 	}
 	
@@ -64,6 +74,8 @@ public class GamePanel extends JPanel implements Runnable{
 									 		// que es JPanel.
 		Graphics2D g2 = (Graphics2D)g;
 		
+		g2.setFont(font);
+		
 		g2.drawImage(logo, 6, 7, 245, 106, this);		// ESTO NO SE TOCA ES EL LOGO
 		
 		// bloques de info
@@ -78,14 +90,24 @@ public class GamePanel extends JPanel implements Runnable{
 		
 		g2.setColor(Color.magenta);			// left temps
 		g2.drawRoundRect(265, 103, 195, 212, 10, 10);
-		
-		g2.setColor(Color.white);			// speed
+		g2.drawRoundRect(270, 108, 185, 99, 10, 10);		//FL
+		g2.drawRoundRect(270, 212, 185, 99, 10, 10);		//RL
+
+		g2.setColor(ISC);					// speed
 		g2.drawRoundRect(465, 103, 311, 212, 10, 10);
+		g2.setColor(Color.white);
+	    g2.drawString("80", 554, 247);
 		
 		g2.setColor(Color.magenta);			// right temps
 		g2.drawRoundRect(781, 103, 195, 212, 10, 10);
+		g2.drawRoundRect(786, 108, 185, 99, 10, 10);		//FR
+		g2.drawRoundRect(786, 212, 185, 99, 10, 10);		//RR
 		
-		g2.setColor(Color.yellow);				// SoC
+		g2.setColor(ISC);					// SoC
 		g2.drawRoundRect(265, 320, 712, 60, 10, 10);
+		g2.fillRoundRect(265, 320, 450, 60, 10, 10);
+	    g2.setColor(Color.white);
+		g2.setFont(font2);
+	    g2.drawString("60%", 590, 370);	    	    
 	}
 }
