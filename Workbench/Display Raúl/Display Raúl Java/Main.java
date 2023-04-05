f485852b7d3e5687184d03bd3e42d525a55de89f
Raúl <201800269@alu.comillas.edu>
2023-04-06 01:12:53 +0200

    Update Main.java

--- Workbench/Display Raúl/Display Raúl Java/Main.java
+++ Workbench/Display Raúl/Display Raúl Java/Main.java
@@ -1,14 +1,24 @@
 import java.awt.*;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
 
 public class Main {
 	public static void main(String argv[]) {
 	
 		JFrame window = new JFrame();
 		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		GraphicsEnvironment ge = GraphicsEnvironment.getLocalGraphicsEnvironment();
+        GraphicsDevice[] devices = ge.getScreenDevices();
+        GraphicsDevice device = devices[0];
+		device.setFullScreenWindow(window); 	
+
 		window.setResizable(false);
 		window.setTitle("Display");
+		
+		
 		
 		GamePanel gamePanel = new GamePanel();
 		window.add(gamePanel);
